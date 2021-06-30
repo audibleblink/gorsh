5586814deb95d9c5b267ea7d3119c57c86e1f637
audibleblink <domain@me.com>
2021-06-30 17:27:21 -0400

    fix listener scripts

--- scripts/listen.sh
+++ scripts/listen.sh
@@ -15,7 +15,7 @@ tmux set-hook -g session-created 'set remain-on-exit on'
 tmux has-session -t "${NAME}" || tmux new-session -d -s "${NAME}"
 
 # Start the listener in a tmux pane, using the new Unix domain socket, and wait
-tmux new-window -t "${NAME}" -a -n "shell" "stty -icanon -brkint -isig && build/srv/gorsh-listen -s ${SOCKF}"
+tmux new-window -t "${NAME}" -a -n "shell" "stty -icanon -brkint -isig && build/srv/gorsh-server -s ${SOCKF}"
 sleep 1
 
 # Hook up the STDIN from the calling socat instance to the Unix socket that gorsh is listening to
