e95d7858c1d74b01df7971ce98b5a5289bd558c6
Alex Flores <domain@me.com>
2022-02-13 23:43:17 -0500

    id_windows

---
+++ internal/cmds/id_windows.go
@@ -0,0 +1,27 @@
+package cmds
+
+import (
+	"runtime"
+
+	"github.com/abiosoft/ishell"
+	"github.com/audibleblink/getsystem"
+	"github.com/audibleblink/gorsh/internal/sitrep"
+	"golang.org/x/sys/windows"
+)
+
+func Id(c *ishell.Context) {
+	output, err := sitrep.UserInfo()
+	if err != nil {
+		c.Println(err)
+		return
+	}
+
+	procH := windows.GetCurrentProcessToken()
+	output.Token, err = getsystem.TokenOwner(procH)
+	if err != nil {
+		c.Println(err)
+		return
+	}
+
+	c.Println(output.String())
+}
